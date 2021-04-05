d448bb38836dbccb119971f3b638d65ccbd29455
Jader <jadergedeon@gmail.com>
2021-04-05 19:32:00 -0300

    Inimigo Adicionado

---
+++ PI Fish Game/Assets/Scripts/Vida_Geral.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida_Geral : MonoBehaviour
+{
+    public delegate void FimDaVidaDelegate();
+    public FimDaVidaDelegate NofimDaVida;
+    public delegate void FeedBack_Visual();
+    public FimDaVidaDelegate FeedBack_Dano;
+    public float totalVida;
+
+    public void Dano(float total)
+    {
+        totalVida -= total;
+
+        FeedBack_Dano();
+
+        if (totalVida <= 0)
+            NofimDaVida();
+
+    }
+}
